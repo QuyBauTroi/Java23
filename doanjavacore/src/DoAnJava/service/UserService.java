9400f34ec21abccd27e31d7dc6d2748006e7829c
QuyBauTroi <nguyenduyquy2108@gmail.com>
2023-11-26 17:43:27 +0700

    ..

--- doanjavacore/src/DoAnJava/service/UserService.java
+++ doanjavacore/src/DoAnJava/service/UserService.java
@@ -9,46 +9,83 @@ import DoAnJava.entities.User;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserService {
     Utils utils=new Utils();
 
     // Phần đăng ký tài khoản người dùng ( chỉ người dùng được đăng ký )
-    public void inputRegister(Scanner scanner, ArrayList<User> users){
+    public void inputRegister(Scanner scanner, ArrayList<User> users) {
         System.out.println("ĐĂNG KÝ");
         System.out.print("Mời bạn nhập username: ");
-        String username=scanner.nextLine();
-        User user=new User();
+        String username = scanner.nextLine();
+        User user = new User();
         user.setUsername(username);
         user.setRole(2);
-        if (checkUsername(username,users)){
-            inputRegister(scanner,users);
-        }else {
-            System.out.print("Mời bạn nhập email: ");
-            String email = scanner.nextLine();
-            if (checkEmail(email,users)){
-                inputRegister(scanner,users);
-            }else {
-                user.setEmail(email);
-                System.out.print("Mời bạn nhập password: ");
-                String password=scanner.nextLine();
-                if(checkPassword(password)){
-                    inputRegister(scanner,users);
-                }else{
-                    user.setPassword(password);
-                    System.out.println("Xác thực thông tin tài khoản");
-                    System.out.print("Mời bạn nhập tên: ");
-                    user.setName(scanner.nextLine());
-                    System.out.print("Mời bạn nhập SĐT: ");
-                    user.setPhoneNumber(scanner.nextLine());
-                    System.out.print("Mời bạn nhập địa chỉ: ");
-                    user.setAddress(scanner.nextLine());
-                    users.add(user);
-                    System.out.println("Đăng ký thành công!!!");
-                    }
+
+        if (checkUsername(username, users)) {
+            inputRegister(scanner, users);
+        } else {
+            // try-catch email
+            String email = null;
+            boolean invalidEmail;
+            do {
+                try {
+                    System.out.print("Mời bạn nhập email: ");
+                    email = scanner.nextLine();
+                    if (checkEmail(email, users)) {throw new Exception("Email đã tồn tại. Vui lòng nhập lại");}
+                    if (!isValidEmail(email)) {throw new Exception("Email không hợp lệ. Vui lòng nhập lại.");}
+                    invalidEmail = false;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    invalidEmail = true;
                 }
-            }
+            } while (invalidEmail);
+            user.setEmail(email);
+
+            // try-catch password
+            String password = null;
+            boolean invalidPassword;
+            do {
+                try {
+                    System.out.print("Mời bạn nhập password: ");
+                    password = scanner.nextLine();
+                    if (!isValidPassword(password)) {
+                        throw new Exception("Password không hợp lệ. Vui lòng nhập lại");
+                    }
+                    invalidPassword = false; // Mật khẩu hợp lệ nếu không có exception
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    invalidPassword = true;
+                }
+            } while (invalidPassword);
+            user.setPassword(password);
+
+            System.out.println("Xác thực thông tin tài khoản");
+            System.out.print("Mời bạn nhập tên: ");
+            user.setName(scanner.nextLine());
+            String phoneNumber;
+            do {
+                System.out.print("Nhập số điện thoại: ");
+                phoneNumber = scanner.nextLine();
+
+                if (isValidPhoneNumber(phoneNumber)) {
+                    user.setPhoneNumber(phoneNumber);
+                    System.out.println("Số điện thoại hợp lệ.");
+                    break;
+                } else {
+                    System.out.println("Số điện thoại không hợp lệ. Vui lòng nhập lại.");
+                }
+            } while (true);
+
+            System.out.print("Mời bạn nhập địa chỉ: ");
+            user.setAddress(scanner.nextLine());
+            users.add(user);
+            System.out.println("Đăng ký thành công!!!");
         }
+    }
+
 
 
 
@@ -56,39 +93,73 @@ public class UserService {
 
 
     //  - Phần đăng ký tài khoản nhân viên mới ( chỉ dược tạo bởi Quản lý)
-    public void inputRegisterStaff(Scanner scanner, ArrayList<User> users){
-        System.out.println("ĐĂNG KÝ TÀI KHOẢN NHÂN VIÊN");
-        System.out.print("Mời bạn nhập username:");
-        String username=scanner.nextLine();
-        User user=new User();
+    public void inputRegisterStaff(Scanner scanner, ArrayList<User> users) {
+        System.out.println("ĐĂNG KÝ");
+        System.out.print("Mời bạn nhập username: ");
+        String username = scanner.nextLine();
+        User user = new User();
         user.setUsername(username);
-        user.setRole(1);
-        if (checkUsername(username,users)){
-            inputRegister(scanner,users);
-        }else {
-            System.out.print("Mời bạn nhập email:");
-            String email = scanner.nextLine();
-            if (checkEmail(email,users)){
-                inputRegister(scanner,users);
-            }else {
-                user.setEmail(email);
-                System.out.print("Mời bạn nhập password:");
-                String password=scanner.nextLine();
-                if(checkPassword(password)){
-                    inputRegister(scanner,users);
-                }else{
-                    user.setPassword(password);
-                    System.out.println("Xác thực thông tin tài khoản");
-                    System.out.print("Mời bạn nhập tên của nhân viên:");
-                    user.setName(scanner.nextLine());
-                    System.out.print("Mời bạn nhập SĐT của nhân viên:");
-                    user.setPhoneNumber(scanner.nextLine());
-                    System.out.print("Mời bạn nhập địa chỉ của nhân viên:");
-                    user.setAddress(scanner.nextLine());
-                    users.add(user);
-                    System.out.println("Đăng ký thành công!!!");
+        user.setRole(2);
+
+        if (checkUsername(username, users)) {
+            inputRegister(scanner, users);
+        } else {
+            // try-catch email
+            String email = null;
+            boolean invalidEmail;
+            do {
+                try {
+                    System.out.print("Mời bạn nhập email: ");
+                    email = scanner.nextLine();
+                    if (checkEmail(email, users)) {throw new Exception("Email đã tồn tại. Vui lòng nhập lại");}
+                    if (!isValidEmail(email)) {throw new Exception("Email không hợp lệ. Vui lòng nhập lại.");}
+                    invalidEmail = false;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    invalidEmail = true;
                 }
-            }
+            } while (invalidEmail);
+            user.setEmail(email);
+
+            // try-catch password
+            String password = null;
+            boolean invalidPassword;
+            do {
+                try {
+                    System.out.print("Mời bạn nhập password: ");
+                    password = scanner.nextLine();
+                    if (!isValidPassword(password)) {
+                        throw new Exception("Password không hợp lệ. Vui lòng nhập lại");
+                    }
+                    invalidPassword = false; // Mật khẩu hợp lệ nếu không có exception
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                    invalidPassword = true;
+                }
+            } while (invalidPassword);
+            user.setPassword(password);
+
+            System.out.println("Xác thực thông tin tài khoản");
+            System.out.print("Mời bạn nhập tên: ");
+            user.setName(scanner.nextLine());
+            String phoneNumber;
+            do {
+                System.out.print("Nhập số điện thoại: ");
+                phoneNumber = scanner.nextLine();
+
+                if (isValidPhoneNumber(phoneNumber)) {
+                    user.setPhoneNumber(phoneNumber);
+                    System.out.println("Số điện thoại hợp lệ.");
+                    break;
+                } else {
+                    System.out.println("Số điện thoại không hợp lệ. Vui lòng nhập lại.");
+                }
+            } while (true);
+
+            System.out.print("Mời bạn nhập địa chỉ: ");
+            user.setAddress(scanner.nextLine());
+            users.add(user);
+            System.out.println("Đăng ký thành công!!!");
         }
     }
 
@@ -123,7 +194,7 @@ public class UserService {
         ManagerAccountService managerAccountService = new ManagerAccountService();
         CustomerService customerService = new CustomerService();
         String select="";
-        boolean isUserNameRight = false;
+        boolean isUserNameTrue = false;
         boolean isContinue = true;
         do {
             System.out.println("ĐĂNG NHẬP");
@@ -133,9 +204,9 @@ public class UserService {
                 if (username.equals(userValue.getUsername())) {
                     System.out.print("Mời bạn nhập password:");
                     String password = scanner.nextLine();
-                    isUserNameRight = true;
+                    isUserNameTrue = true;
                     if (password.equals(userValue.getPassword())) {
-                        System.out.println("Đăng nhập thành công!!!");
+                        System.out.println("Đăng nhập thành công");
                         if (userValue.getRole()==1){
                             staffAccountService.menuStaff(scanner,users,userValue,userService,productMap,orders);
                         }else if (userValue.getRole()==2){
@@ -148,7 +219,7 @@ public class UserService {
                         System.out.println("Mời bạn chọn:");
                         System.out.println("1-Đăng nhập lại");
                         System.out.println("2-Quên mật khẩu");
-                        System.out.print("Mời bạn chọn:");
+                        System.out.print("Enter your choice:");
                         int choice = utils.inputInt(scanner);
                         switch (choice) {
                             case 1 -> isContinue = inputLogin(scanner, users, userService, productMap, orders);
@@ -158,7 +229,7 @@ public class UserService {
                     break;
                 }
             }
-            if(!isUserNameRight){
+            if(!isUserNameTrue){
                 System.out.println("Kiểm tra lại username");
                 System.out.print("Bạn có muốn nhập lại không(Y/N): ");
                 select = scanner.nextLine();
@@ -290,7 +361,7 @@ public class UserService {
             try {
                 System.out.print("Mời bạn nhập password mới:");
                 String password = scanner.nextLine();
-                boolean isError = checkPassword(password);// Kiểm tra định dạng password
+                boolean isError = isValidPassword(password);// Kiểm tra định dạng password
                 if (!isError) {
                     user.setPassword(password);
                     validPassword = true;
@@ -346,31 +417,41 @@ public class UserService {
            }
     return isError;
     }
-    public boolean checkEmail(String email, ArrayList<User> users){
-        boolean isError =false;
-        String regex = "^(.+)@(.+)$";
-        if(email.matches(regex)){
-            for (User userValue : users){
-                if (userValue.getEmail().equals(email)){
-                    isError = true;
-                    System.out.println("Email này ton tai, vui long nhap lai");
-                    break;
-                }
-            }
-        }else{
-            System.out.println("Sai định dạng Email, vui long nhập lại ");
-            isError = true;
-        }
-        return isError;
+    private boolean isValidEmail(String email) {
+        // Regex cho địa chỉ email hợp lệ
+        String regex = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
     }
-    private boolean checkPassword(String password) {
-        String regex = "^(?=.*[A-Z])(?=.*[.,-_;])[A-Za-z.,-_;]{7,15}$";
+    private boolean checkEmail(String email, ArrayList<User> users) {
         boolean isError = false;
-        if (!password.matches(regex)) {
-            System.out.println("Mật khẩu sai định dạng , vui lòng nhập lại");
-            isError = true;
+        for (User userValue : users) {
+            if (userValue.getEmail().equals(email)) {
+                isError = true;
+                break;
+            }
         }
         return isError;
     }
 
+    private  boolean isValidPassword(String password) {
+        // Regex cho địa chỉ email hợp lệ
+        String regex = "^(?=.*[A-Z])(?=.*[.,-_;])[A-Za-z.,-_;]{7,15}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
+
+
+    // Phương thức kiểm tra số điện thoại bằng regex
+    private  boolean isValidPhoneNumber(String phoneNumber) {
+        // Regex cho số điện thoại có định dạng 10 chữ số (không tính ký tự đặc biệt)
+        String regex = "^[0-9]{10}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        return matcher.matches();
+    }
 }
